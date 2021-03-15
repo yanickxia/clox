b91832c29fee291fcc8ce31dbc1c4fa0972e5fea
yanick <yanick.xia@gmail.com>
2021-03-15 17:08:20 +0800

    Add chuck of bytecode

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
+#include "chunk.h"
 #include "common.h"
+#include "debug.h"
 
 int main(int argc, const char *argv[]) {
-    printf("hello, world");
-    return 0; 
+  Chunk chuck;
+  Chunk chunk;
+  initChunk(&chunk);
+
+  int constant = addConstant(&chunk, 1.2);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant,123);
+
+  writeChunk(&chunk, OP_RETURN, 123);
+
+  disassembleChunk(&chunk, "test chunk");
+
+  freeChunk(&chunk);
+  return 0;
 }
